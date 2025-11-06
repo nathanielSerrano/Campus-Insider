d429ebfa7e46714e106de86507701645f6c0f246
bennyyy51 <126090605+bennyyy51@users.noreply.github.com>
2025-11-06 16:15:55 -0500

    Adding the campus location schema

---
+++ Phase 2/schema-implementation/campusInsider.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE campus_insider;
+USE campus_insider;
+
+CREATE TABLE university (
+    university_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE campus (
+  campus_name VARCHAR(100),
+  university_id INT NOT NULL,
+  PRIMARY KEY (campus_name, university_id),
+  FOREIGN KEY (university_id) REFERENCES university(university_id)
+);
+
+CREATE TABLE location (
+    LID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    image LONGBLOB,
+    campus_name VARCHAR(100) NOT NULL,
+    university_id INT NOT NULL,
+    FOREIGN KEY (campus_name, university_id)
+        REFERENCES campus(campus_name, university_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE nonbuildings (
+    LID INT PRIMARY KEY,
+    description TEXT,
+    FOREIGN KEY (LID) REFERENCES location(LID) ON DELETE CASCADE
+);
+
+CREATE TABLE buildings (
+    LID INT PRIMARY KEY,
+    FOREIGN KEY (LID) REFERENCES location(LID) ON DELETE CASCADE
+);
+
+CREATE TABLE rooms (
+    LID INT PRIMARY KEY,
+    building_LID INT NOT NULL,
+    room_number VARCHAR(10) NOT NULL,
+    room_type ENUM('study room', 'computer room', 'science lab', 'classroom', 'facility', 'meeting room', 'store', 'venue') NOT NULL,
+    room_size ENUM('small', 'medium', 'large') NOT NULL,
+    FOREIGN KEY (LID) REFERENCES location(LID) ON DELETE CASCADE,
+    FOREIGN KEY (building_LID) REFERENCES buildings(LID) ON DELETE CASCADE
+);
