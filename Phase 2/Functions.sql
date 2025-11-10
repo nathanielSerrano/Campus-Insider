fe54253036293b11f1af6ca840f20ecdf21edbfd
Ahmad Mouhsen <ahmad.mouhsen@maine.edu>
2025-11-10 09:38:32 -0500

    Created Get University ID BY Name Function

---
+++ Phase 2/Functions.sql
@@ -0,0 +1,19 @@
+# Functions, run the functions first before the procedures since they are used by the them.
+ 
+DELIMITER $$
+CREATE FUNCTION GetUniversityIDByName(p_name VARCHAR(100))
+RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE univ_id INT;
+
+    SELECT university_id
+    INTO univ_id
+    FROM university
+    WHERE name = p_name
+    LIMIT 1;
+
+    RETURN univ_id;
+END$$
+DELIMITER ;
