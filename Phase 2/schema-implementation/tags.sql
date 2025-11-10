671c55dadbd6c68a89b6a2ec5f0571a9d22a0b19
bennyyy51 <126090605+bennyyy51@users.noreply.github.com>
2025-11-10 10:07:35 -0500

    Change rating_id to RID in tags.sql
    for readability and changes in ratings table

--- Phase 2/schema-implementation/tags.sql
+++ Phase 2/schema-implementation/tags.sql
@@ -1,13 +1,13 @@
 CREATE TABLE rating_equipment (
-    rating_id INT NOT NULL,
+    RID INT NOT NULL,
     equipment_tag VARCHAR(100) NOT NULL,
-    PRIMARY KEY (rating_id, equipment_tag),
-    FOREIGN KEY (rating_id) REFERENCES ratings(rating_id) ON DELETE CASCADE
+	PRIMARY KEY (RID, equipment_tag),
+    FOREIGN KEY (RID) REFERENCES ratings(RID) ON DELETE CASCADE
 );
 
 CREATE TABLE rating_accessibility (
-    rating_id INT NOT NULL,
+    RID INT NOT NULL,
     accessibility_tag VARCHAR(100) NOT NULL,
-    PRIMARY KEY (rating_id, accessibility_tag),
-    FOREIGN KEY (rating_id) REFERENCES ratings(rating_id) ON DELETE CASCADE
+    PRIMARY KEY (RID, accessibility_tag),
+    FOREIGN KEY (RID) REFERENCES ratings(RID) ON DELETE CASCADE
 );
