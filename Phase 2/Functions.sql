60c5254ce60d09e0d5206b16a783f1cb4be92402
Ahmad Mouhsen <ahmad.mouhsen@maine.edu>
2025-11-13 21:24:20 -0500

    Update Functions.sql

--- Phase 2/Functions.sql
+++ Phase 2/Functions.sql
@@ -1,7 +1,10 @@
 -- Functions, run the functions first before the procedures since they are used by the them.
  
 DELIMITER $$
-CREATE FUNCTION GetUniversityIDByName(p_name VARCHAR(100))
+CREATE FUNCTION GetUniversityIDByNameAndState(
+    p_name VARCHAR(100),
+    p_state VARCHAR(50)
+)
 RETURNS INT
 DETERMINISTIC
 READS SQL DATA
@@ -12,8 +15,10 @@ BEGIN
     INTO univ_id
     FROM university
     WHERE name = p_name
+      AND state = p_state
     LIMIT 1;
 
     RETURN univ_id;
 END$$
 DELIMITER ;
+
